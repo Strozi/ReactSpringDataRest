8a257c927e65c8d283ee5dad4d85dfb74041f2c8
Michał Stróżyński <michal.strozynski@o2.pl>
2017-06-28 20:50:27 +0200

    Firt commit

---
+++ src/main/java/michu/spring/tutorials/ReactSpringDataRestApplication.java
@@ -0,0 +1,12 @@
+package michu.spring.tutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringDataRestApplication.class, args);
+	}
+}
